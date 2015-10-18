819708e745d748b0fd8a187cbdaf96040693885f
Eisuke Kozu <eisuke@c-styles.jp>
2015-10-18 12:40:17 +0900

    一旦１ファイルに寄せる

--- config/deploy.rb
+++ config/deploy.rb
@@ -21,3 +21,15 @@ task :change_permission, roles => :web do
     chmod -R #{deploy_to}/current/app/tmp
   CMD
 end
+
+role :web, *%w[
+  localhost
+]
+role :db, *%w[
+  localhost
+]
+
+set :user, "vagrant"
+set(:password) do
+  Capistrano::CLI.password_prompt "Set password for develop server: "
+end
--- config/deploy/develop.rb
+++ config/deploy/develop.rb
@@ -1,12 +1,1 @@
 # develop                                                                                                                                                                                                          #
-role :web, *%w[
-  localhost
-]
-role :db, *%w[
-  localhost
-]
-
-set :user, "vagrant"
-set(:password) do
-  Capistrano::CLI.password_prompt "Set password for develop server: "
-end
